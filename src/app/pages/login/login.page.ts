036e17fed0ff8ef6dfe0c4ee92050ab04620adef
Mike-Labs <mikemawirabet@gmail.com>
2025-04-27 19:39:13 +0300

    second commit
    Made code clean

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -50,32 +50,3 @@ export class LoginPage implements OnInit {
     }
   }
 }
-
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-// import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.page.html',
-//   styleUrls: ['./login.page.scss'],
-//   standalone: true,
-//   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ReactiveFormsModule]
-// })
-// export class LoginPage implements OnInit {
-
-
-
-//   constructor() { }
-
-//   ngOnInit() {
-    
-//   }
-
-
-// }

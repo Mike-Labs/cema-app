73759271830517b8372ee68db2fdf7e1cae1715e
Mike-Labs <mikemawirabet@gmail.com>
2025-04-28 01:21:56 +0300

    Updated production for firebase
    Updated production for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCApOCIYySxMJbsQL6vZYMl5MY497ki21s',
+    authDomain: 'cema-app-33c96.firebaseapp.com',
+    projectId: 'cema-app-33c96',
+    storageBucket: 'cema-app-33c96.firebasestorage.app',
+    messagingSenderId: '893259610281',
+    appId: '1:893259610281:web:52661db4751081c0f8d1b8'
+  }
 };
+
